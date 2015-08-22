db29889c1af219416714e079d6fe6f01adf1190a
Shinmera <shinmera@tymoon.eu>
2015-08-22 23:57:16 +0200

    Package update

--- package.lisp
+++ package.lisp
@@ -10,17 +10,20 @@
   (:use #:cl)
   (:export
    #:vec
+   #:vec=
    #:copy
    #:x
    #:y
    #:z
    #:size
    #:translate
+   #:-translate
    #:scale
    #:rotate-around
    #:rotate
    #:normalize
    #:translated
+   #:-translated
    #:scaled 
    #:rotated
    #:normalized
@@ -71,4 +74,127 @@
 
 (defpackage #:flare
   (:nicknames #:org.shirakumo.flare)
-  (:use #:cl #:flare-vector #:flare-queue #:flare-indexed-set))
+  (:use #:cl #:flare-vector #:flare-queue #:flare-indexed-set)
+  ;; animation.lisp
+  (:export
+   #:tick
+   #:add-animation
+   #:remove-animation
+   #:compile-change
+   #:perform
+   #:initial-value
+   #:animations
+   #:start
+   #:end
+   #:selector
+   #:changes
+   #:field
+   #:from
+   #:to
+   #:from
+   #:by
+   #:ease-func
+   #:progression
+   #:animation
+   #:change
+   #:tween
+   #:scale
+   #:rotate
+   #:set!
+   #:increase
+   #:edit
+   #:enter
+   #:leave
+   #:delegating-change
+   #:every.)
+  ;; easings.lisp
+  (:export
+   #:easing
+   #:remove-easing
+   #:define-easing
+   #:ease
+   #:ease-vec
+   #:linear
+   #:quad-in
+   #:quad-out
+   #:quad-in-out
+   #:cubic-in
+   #:cubic-out
+   #:cubic-in-out
+   #:quart-in
+   #:quart-out
+   #:quart-in-out
+   #:quint-in
+   #:quint-out
+   #:quint-in-out
+   #:sine-in
+   #:sine-out
+   #:sine-in-out
+   #:expo-in
+   #:expo-out
+   #:expo-in-out
+   #:circ-in
+   #:circ-out
+   #:circ-in-out
+   #:back-in
+   #:back-out
+   #:back-in-out
+   #:elastic-in
+   #:elastic-out
+   #:elastic-in-out
+   #:bounce-in
+   #:bounce-out
+   #:bounce-in-out)
+  ;; parser.lisp
+  (:export
+   #:define-progression)
+  ;; scene.lisp
+  (:export
+   #:call-with-translation
+   #:with-translation
+   #:update
+   #:stop
+   #:running
+   #:start
+   #:reset
+   #:clock
+   #:visibility
+   #:paint
+   #:objects
+   #:insert
+   #:withdraw
+   #:name-map
+   #:units
+   #:unit
+   #:enter
+   #:leave
+   #:object
+   #:name
+   #:collective
+   #:progressions
+   #:add-progression
+   #:remove-progression
+   #:scene
+
+   #:target
+   #:timer
+   #:paintable
+   #:container
+   #:map-container-tree
+   #:do-container-tree
+   #:print-container-tree
+   #:collective
+   #:unit
+   #:scene
+   #:scene-unit
+   #:entity
+   #:oriented-entity
+   #:sized-entity
+   #:formation
+   #:particle
+   #:ring
+   #:location
+   #:orientation
+   #:size
+   #:up
+   #:angle))
