76c7bb183d75a52d50537b7a8e884e0c506a7040
Shinmera <shinmera@tymoon.eu>
2015-08-23 10:52:05 +0200

    Minor changes to the default scene, removed other stuff to move to individual files.

--- scene.lisp
+++ scene.lisp
@@ -6,26 +6,6 @@
 
 (in-package #:org.shirakumo.flare)
 
-(defgeneric call-with-translation (func target vec))
-(defgeneric update (object))
-(defgeneric stop (timer))
-(defgeneric running (timer))
-(defgeneric start (timer))
-(defgeneric reset (timer))
-(defgeneric clock (timer))
-(defgeneric visibility (paintable))
-(defgeneric paint (paintable target))
-(defgeneric objects (container))
-(defgeneric insert (container &rest objects))
-(defgeneric withdraw (container &rest objects))
-(defgeneric name-map (collective))
-(defgeneric units (collective))
-(defgeneric unit (name collective))
-(defgeneric enter (unit collective))
-(defgeneric leave (unit collective))
-(defgeneric object (collective))
-(defgeneric name (unit))
-(defgeneric collective (unit))
 (defgeneric progressions (scene))
 (defgeneric add-progression (progression scene))
 (defgeneric remove-progression (progression scene))
@@ -36,161 +16,52 @@
 (defgeneric up (entity))
 (defgeneric angle (entity))
 
-(defmethod update (object)
-  object)
-
-(defmethod paint (paintable target)
-  paintable)
-
-(defclass target ()
-  ())
-
-(defmacro with-translation ((vec target) &body body)
-  `(call-with-translation (lambda () ,@body) ,target ,vec))
-
-(defclass timer ()
-  ((previous-time :initform NIL :accessor previous-time)
-   (clock :initarg :clock :accessor clock))
-  (:default-initargs :clock 0.0s0))
-
-(defmethod print-object ((timer timer) stream)
-  (print-unreadable-object (timer stream :type T :identity T)
-    (format stream "~s ~a" (if (previous-time timer) :running :stopped) (clock timer))))
-
-(defmethod running ((timer timer))
-  (not (null (previous-time timer))))
-
-(defmethod reset ((timer timer))
-  (setf (clock timer) 0.0s0)
-  (when (previous-time timer)
-    (setf (previous-time timer) (get-internal-real-time)))
-  timer)
-
-(defmethod stop ((timer timer))
-  (setf (previous-time timer) NIL)
-  timer)
-
-(defmethod start ((timer timer))
-  (setf (previous-time timer) (get-internal-real-time))
-  timer)
-
-(defmethod update :before ((timer timer))
-  (let ((new-time (get-internal-real-time)))
-    (incf (clock timer)
-          (float (/ (- new-time (previous-time timer))
-                    internal-time-units-per-second)
-                 1.0s0))
-    (setf (previous-time timer) new-time)))
-
-(defmethod update :around ((timer timer))
-  (when (running timer)
-    (call-next-method)))
-
-(defclass paintable ()
-  ((visibility :initarg :visibility :accessor visibility))
-  (:default-initargs :visibility 1.0))
-
-(defclass container ()
-  ((objects :initform (make-indexed-set) :accessor objects)))
-
-(defun map-container-tree (function container)
-  (labels ((traverse (container)
-             (when (typep container 'container)
-               (do-set (i container) (objects container)
-                 (declare (ignore i))
-                 (funcall function container)
-                 (traverse container)))))
-    (traverse container)))
-
-(defmacro do-container-tree ((object container) &body body)
-  `(block NIL (map-container-tree (lambda (,object) ,@body) ,container)))
-
-(defun print-container-tree (container)
-  (labels ((print-container (container level)
-             (format T "~&~a ~a~%" (make-string (* level 2) :initial-element #\ ) container)
-             (when (typep container 'container)
-               (do-set (i container) (objects container)
-                 (declare (ignore i))
-                 (print-container container (1+ level))))))
-    (print-container container 0)))
-
-(defmethod insert ((container container) &rest objects)
-  (dolist (obj objects)
-    (set-add obj (objects container)))
-  container)
-
-(defmethod withdraw ((container container) &rest objects)
-  (dolist (obj objects)
-    (set-remove obj (objects container)))
-  container)
-
-(defmethod paint ((container container) target)
-  (do-set (i obj) (objects container)
-    (declare (ignore i))
-    (paint obj target)))
-
-(defclass collective (container)
-  ((name-map :initform (make-hash-table :test 'eql) :accessor name-map)))
-
-(defmethod units ((collective collective))
-  (objects collective))
-
-(defmethod unit (name (collective collective))
-  (gethash name (name-map collective)))
-
-(defclass unit ()
-  ((name :initarg :name :accessor name)
-   (collective :initarg :collective :accessor collective))
-  (:default-initargs
-   :name NIL
-   :collective NIL))
-
-(defmethod (setf name) :before (name (unit unit))
-  (when (collective unit)
-    (when (name unit)
-      (remhash (name unit) (name-map (collective unit))))
-    (when name
-      (setf (gethash name (name-map (collective unit))) unit))))
-
-(defmethod enter ((unit unit) (container container))
-  (insert container unit))
-
-(defmethod enter ((unit unit) (collective collective))
-  (call-next-method)
-  (setf (collective unit) collective)
-  (setf (gethash (name unit) (name-map collective)) unit))
-
-(defmethod leave ((unit unit) (container container))
-  (withdraw container unit))
-
-(defmethod leave ((unit unit) (collective collective))
-  (call-next-method)
-  (setf (collective unit) NIL)
-  (remhash (name unit) (name-map collective)))
-
-(defmethod leave ((unit unit) (collective (eql T)))
-  (leave unit (collective unit)))
-
-(defmethod unit (name (unit unit))
-  (unit name (collective unit)))
-
-(defclass scene (collective timer paintable)
+(defclass scene (collective clock paintable)
   ((progressions :initform (make-hash-table :test 'eql) :accessor progressions)))
 
+(defmethod describe-object ((scene scene) stream)
+  (format stream "~a
+  [~a]
+
+Internal clock is at ~a
+Progressions:
+  ~{~a~^~%  ~}
+Tree:"
+          scene (type-of scene) (clock scene)
+          (loop for v being the hash-values of (progressions scene) collect v))
+  (print-container-tree scene stream))
+
+(defmethod start :after ((scene scene))
+  (loop for progression being the hash-values of (progressions scene)
+        do (setf (previous-time progression) (previous-time scene))))
+
 (defmethod update ((scene scene))
-  (maphash (lambda (name progression)
-             (declare (ignore name))
-             (tick progression scene))
-           (progressions scene)))
+  (loop for progression being the hash-values of (progressions scene)
+        do (tick progression scene)))
+
+(defmethod paint ((scene scene) target)
+  (do-set (i obj) (objects scene)
+    (declare (ignore i))
+    (paint obj target)))
 
 (defmethod progression (name (scene scene))
   (gethash name (progressions scene)))
 
-(defmethod add-progression (progression (scene scene))
+(defmethod add-progression ((name symbol) (scene scene))
+  (add-progression (make-instance 'progression :name name) scene))
+
+(defmethod add-progression ((progression progression) (scene scene))
+  (when (eql progression (gethash (name progression) (progressions scene)))
+    (cerror "Replace the progression."
+            "A different progression with the same name ~s already exists on ~a."
+            (name progression) scene))
   (setf (gethash (name progression) (progressions scene)) progression))
 
-(defmethod remove-progression (progression (scene scene))
-  (remhash (name progression) (progressions scene)))
+(defmethod remove-progression ((name symbol) (scene scene))
+  (remhash name (progressions scene)))
+
+(defmethod remove-progression ((progression progression) (scene scene))
+  (remove-progression (name progression) scene))
 
 (defclass scene-unit (unit)
   ())
@@ -205,6 +76,11 @@
   ((location :initarg :location :accessor location))
   (:default-initargs
    :location (vec 0 0 0)))
+
+(defmethod paint ((entity entity) target)
+  (do-set (i obj) (objects entity)
+    (declare (ignore i))
+    (paint obj target)))
 
 (defmethod enter ((entity entity) (container entity))
   (call-next-method)
