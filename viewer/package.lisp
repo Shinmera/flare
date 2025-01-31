8fdb8158d1cb63431b7e4740055052293417ad1f
Yukari Hafner <shinmera@tymoon.eu>
2025-01-31 09:38:10 +0100

    Delete the viewer since qtools is gone.

--- viewer/package.lisp
+++
@@ -1,6 +0,0 @@
-(in-package #:cl-user)
-(defpackage #:flare-viewer
-  (:nicknames #:org.shirakumo.flare.viewer)
-  (:use #:cl+qt #:3d-vectors #:flare)
-  (:shadowing-import-from #:flare #:slot)
-  (:export #:main))
